3cb23f93a717af482b9af982bf6b02ad5afb44c3
Yu Li <yu.li@ipt.ch>
2018-06-22 13:40:14 +0200

    Test

---
+++ GRadarHelsana/ScoreAPI/Models/Score.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoreAPI.Models
+{
+    public class Score
+    {
+        public string GeneralRiskScore { get; set; }
+
+        public string CombinedRiskScore { get; set; }
+    }
+}
