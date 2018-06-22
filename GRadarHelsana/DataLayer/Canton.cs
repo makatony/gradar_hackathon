0deacd58ec21da8ba025cda81d3d862bb7ee680b
Attila Ujhazi <qb12@hotmail.ch>
2018-06-22 13:07:31 +0200

    Added DataLAyer

---
+++ GRadarHelsana/DataLayer/Canton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    static class Canton
+    {
+    }
+
+    public enum KT
+    {
+        AG,
+        AI,
+        AR,
+        BE,
+        BL,
+        BS,
+        FR,
+        GE,
+        GL,
+        GR,
+        JU,
+        LU,
+        NE,
+        NW,
+        OW,
+        SG,
+        SH,
+        SO,
+        SZ,
+        TG,
+        TI,
+        UR,
+        VD,
+        VS,
+        ZG,
+        ZH
+    }
+
+}
---
+++ GRadarHelsana/GRadarHelsana/BAG_Adapter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using DataLayer;
+
+namespace GRadarHelsana
+{
+    public class BAG_Adapter
+    {
+        List<BAGData> list = new List<BAGData>();
+        string fileURL = "https://www.bag.admin.ch/bag/de/home/themen/mensch-gesundheit/uebertragbare-krankheiten/ausbrueche-epidemien-pandemien/aktuelle-ausbrueche-epidemien/saisonale-grippe---lagebericht-schweiz/_jcr_content/par/externalcontent.external.exturl.csv/aHR0cDovL3d3dy5iYWctYW53LmFkbWluLmNoLzIwMTZfbWVsZG/VzeXN0ZW1lL3NlbnRpbmVsbGEvaW5mbHVlbnphZGF0ZW4vZC9i/YWdfaW5mbHVlbnphX3JlZ2lvbi5jc3Y_d2ViZ3JhYj1pZ25vcm/U=.csv";
+
+        internal void GetData()
+        {
+            WebClient wc = new WebClient();
+            using (MemoryStream stream = new MemoryStream(wc.DownloadData(fileURL)))
+            {
+                ParseData(stream);
+            }
+        }
+
+
+        internal void ParseData(MemoryStream stream)
+        {
+            StreamReader reader = new StreamReader(stream, System.Text.Encoding.UTF8, true);
+            while (!reader.EndOfStream)
+            {
+
+
+            }
+
+        }
+
+        internal void SaveData()
+        { }
+
+    }
+}
